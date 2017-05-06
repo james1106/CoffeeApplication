22a0d1151fc495a7b7a7a32a40d7ef67515ca71a
linguoding <804961340@qq.com>
2017-05-06 18:38:40 +0800

    完善注册接口，添加成员表是否注册字段，上传图片

--- src/test/java/com/mk/coffee/CreateMenuTest.java
+++ src/test/java/com/mk/coffee/CreateMenuTest.java
@@ -14,14 +14,17 @@ import java.util.List;
  * Created by Administrator on 2017/3/14 0014.
  */
 public class CreateMenuTest extends BaseTestService {
-    private final String oneUrl="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb4de448e44d78c61&redirect_uri=http%3a%2f%2fhuihuisuiyue.apps.mugglecoding.com%2findex&response_type=code&scope=snsapi_base#wechat_redirec";
-    private final String meUrl="http://huihuisuiyue.apps.mugglecoding.com/memberCenter";
+    /*private final String oneUrl="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb4de448e44d78c61&redirect_uri=http%3a%2f%2fhuihuisuiyue.apps.mugglecoding.com%2findex&response_type=code&scope=snsapi_base#wechat_redirec";*/
+
+    private final String oneUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb4de448e44d78c61&redirect_uri=http%3A%2F%2Fmkcaffee.chinaeast.cloudapp.chinacloudapi.cn%2Fapp%2Fcoffees%2Findex.html%23%2FloginByCode&response_type=code&scope=snsapi_base#wechat_redirect";
+    /*private final String meUrl="http://huihuisuiyue.apps.mugglecoding.com/memberCenter";*/
+    private final String meUrl = "http://mkcaffee.chinaeast.cloudapp.chinacloudapi.cn/app/coffees/index.html#/myCoffees";
     @Autowired
     private WxMpService wxMpService;
 
     @Test
     public void menuCreate() throws WxErrorException {
-        /*WxMenu wxMenu = new WxMenu();
+        WxMenu wxMenu = new WxMenu();
         List<WxMenuButton> wxMenuButtons = new ArrayList<>();
         //第一个菜单
         WxMenuButton wxMenuButton1 = new WxMenuButton();
@@ -33,22 +36,22 @@ public class CreateMenuTest extends BaseTestService {
         WxMenuButton wxMenuButton2 = new WxMenuButton();
         wxMenuButton2.setName("E乐饮");
 
-        List<WxMenuButton> subButtons=new ArrayList<>();
-        WxMenuButton subButton1=new WxMenuButton();
+        List<WxMenuButton> subButtons = new ArrayList<>();
+        WxMenuButton subButton1 = new WxMenuButton();
         subButton1.setName("投资合作");
         subButton1.setType("view");
         subButton1.setUrl("http://mp.weixin.qq.com/s/KR5P6KOB8V43rx81rBk9cw");
         subButtons.add(subButton1);
 
 
-        WxMenuButton subButton2=new WxMenuButton();
+        WxMenuButton subButton2 = new WxMenuButton();
         subButton2.setName("关于我们");
         subButton2.setType("view");
         subButton2.setUrl("http://mp.weixin.qq.com/s/WCt1tMoobDjHFNRAnATdrA");
         subButtons.add(subButton2);
 
 
-        WxMenuButton subButton3=new WxMenuButton();
+        WxMenuButton subButton3 = new WxMenuButton();
         subButton3.setName("如何取咖啡");
         subButton3.setType("click");
         subButton3.setKey("unrealized");
@@ -59,8 +62,22 @@ public class CreateMenuTest extends BaseTestService {
         //第三个菜单
         WxMenuButton wxMenuButton3 = new WxMenuButton();
         wxMenuButton3.setName("我的");
-        wxMenuButton3.setType("view");
-        wxMenuButton3.setUrl(meUrl);
+        List<WxMenuButton> subThirdButtons = new ArrayList<>();
+
+        WxMenuButton sub31Button = new WxMenuButton();
+        sub31Button.setName("我的咖啡");
+        sub31Button.setType("view");
+        sub31Button.setUrl("http://mkcaffee.chinaeast.cloudapp.chinacloudapi.cn/app/coffees/index.html#/myCoffees");
+        subThirdButtons.add(sub31Button);
+        wxMenuButton3.setSubButtons(subThirdButtons);
+
+        WxMenuButton sub32Button = new WxMenuButton();
+        sub32Button.setName("我的订单");
+        sub32Button.setType("view");
+        sub32Button.setUrl("http://mkcaffee.chinaeast.cloudapp.chinacloudapi.cn/app/coffees/index.html#/myOrder");
+        subThirdButtons.add(sub31Button);
+        wxMenuButton3.setSubButtons(subThirdButtons);
+
 
 
         //添加菜单
@@ -70,6 +87,6 @@ public class CreateMenuTest extends BaseTestService {
         //设置菜单
         wxMenu.setButtons(wxMenuButtons);
         //创建菜单
-        wxMpService.getMenuService().menuCreate(wxMenu);*/
+        wxMpService.getMenuService().menuCreate(wxMenu);
     }
 }
